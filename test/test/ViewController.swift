98a13f21df7d771eb96562fa0fb8a4799b84d919
左春雷 <86437560@qq.com>
2020-09-29 14:25:57 +0800

    测试了一些信息

---
+++ test/test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by ronie_z on 2020/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.test()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    /// test
+    func test() {
+        print("test")
+    }
+
+}
+
